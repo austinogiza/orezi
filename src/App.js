82d4294b6619b0c1e1bbd38dd8439d80171600b6
austinogiza <austinogiza@gmail.com>
2020-11-16 13:22:37 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import './App.css'
+import img from './assets/black.png'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ <React.Fragment>
+
+   <div className="logo">
+   <div className="image"><img src={img} alt="the creative mena logo"/></div>
+   <div className="title">
+   <h1>The Creative Mena Is Coming Soon</h1>
+
+   </div>
+
+   </div>
+ </React.Fragment>
+  )
 }
 
-export default App;
+export default App
